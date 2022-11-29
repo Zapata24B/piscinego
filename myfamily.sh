25c3291507d5719338151018d67351163c9b6c11
serignmbaye <barsaliou1998@gmail.com>
2022-11-29 23:13:26 +0000

    camp4

---
+++ myfamily.sh
@@ -0,0 +1,4 @@
+returnValue=`curl -s https://learn.zone01dakar.sn/assets/superhero/all.json`
+echo $returnValue  | cat > list.json
+family=$(jq -r --arg HERO_ID $HERO_ID '.[]  | select(.id==($HERO_ID|tonumber)) | .connections.relatives' list.json)
+echo $family
