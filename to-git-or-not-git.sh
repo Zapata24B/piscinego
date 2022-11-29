9539b5d0555e068504515c8c111f59610ac70047
serignmbaye <barsaliou1998@gmail.com>
2022-11-29 18:28:07 +0000

    to git or not to git - correctino

--- to-git-or-not-git.sh
+++
@@ -1,2 +0,0 @@
-returnValue=`curl -s https://learn.zone01dakar.sn/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"serignmbaye\"}}){id}}"}'`
-echo $returnValue | grep -Po '8521'
