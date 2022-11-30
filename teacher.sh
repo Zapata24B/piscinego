05d0ae7bec9cbdf0a9e8a02d9e681efdb5a69d90
serignmbaye <barsaliou1998@gmail.com>
2022-11-30 19:41:56 +0000

    Teacher

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,5 @@
 key=$(grep "Church" -r interviews -l | cut -d "-" -f2)
+key1=$(head -n 179 streets/Buckingham_Place | tail -n 1 | cut -d "#" -f2)
 echo $key
 cat interviews/interview-"$key"
 echo $MAIN_SUSPECT
