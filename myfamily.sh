bb6bd7ff6bd18e38dbf55b57764bd5a047d5e62e
serignmbaye <barsaliou1998@gmail.com>
2022-11-30 00:02:14 +0000

    fix(myfamily) 6

--- myfamily.sh
+++ myfamily.sh
@@ -1,4 +1,1 @@
-returnValue=`curl -s https://learn.zone01dakar.sn/assets/superhero/all.json`
-echo $returnValue  | cat > list.json
-family=$(jq -r --arg HERO_ID $HERO_ID '.[]  | select(.id==($HERO_ID|tonumber)) | .connections.relatives' list.json) 
-echo "$family" | sed 's/"//g'
+curl -s https://learn.zone01dakar.sn/assets/superhero/all.json | jq -r --arg HERO_ID $HERO_ID '.[]  | select(.id==($HERO_ID|tonumber)) | .connections.relatives' | sed 's/"//g'
