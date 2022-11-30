a6fc6307c7df7b3cc8529ea7246258b84f8d36b0
serignmbaye <barsaliou1998@gmail.com>
2022-11-30 19:16:13 +0000

    new method lookagain

--- lookagain.sh
+++ lookagain.sh
@@ -1,2 +1,4 @@
 #!bin/bash
 find . -name "*.sh" | sort -r | rev | cut -d "/" -f1 | rev | sed 's/.sh//g'
+# Explications faites pour la dame voilé
+# find . -name "*.sh" | sort -r | sed 's/.sh//g' | sed 's/.\///g'
