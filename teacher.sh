b94201d3e79c971d10b4f48426f4736ed2c17e7b
serignmbaye <barsaliou1998@gmail.com>
2022-11-30 18:41:13 +0000

    teacher

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,4 @@
-id=$(grep "Church" -r interviews -l | cut -d "-" -f2)
-echo $id
-cat interviews/interview-"$id"
+key=$(grep "Church" -r interviews -l | cut -d "-" -f2)
+echo $key
+cat interviews/interview-"$key"
 echo $MAIN_SUSPECT
