b061e762f211f99d055d77c5dccf1a42f007520e
serignmbaye <barsaliou1998@gmail.com>
2022-12-01 13:26:17 +0000

    print digits - other method 2

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := '0'; i <= '9'; i++ {
-		z01.PrintRune(rune(i))
+	for i := 0; i <= 9; i++ {
+		z01.PrintRune(rune(48 + i))
 	}
 	z01.PrintRune('\n')
 }
