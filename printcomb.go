3cd8d5a6dd8f55ba551abb1fd75071606a0cdc83
serignmbaye <barsaliou1998@gmail.com>
2022-12-01 14:50:28 +0000

    Print Combination

---
+++ printcomb.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := 0; i <= 9; i++ {
+		for j := 0; j <= 9; j++ {
+			for k := 0; k <= 9; k++ {
+				if i != j && i != k && i < j && j < k {
+					z01.PrintRune(rune(48 + i))
+					z01.PrintRune(rune(48 + j))
+					z01.PrintRune(rune(48 + k))
+					z01.PrintRune('\n')
+				}
+			}
+		}
+	}
+}
