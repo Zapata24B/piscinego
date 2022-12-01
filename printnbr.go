3f3632795ba4c5df8a55ca07288809f3ec7f8024
serignmbaye <barsaliou1998@gmail.com>
2022-12-01 19:01:42 +0000

    Print Number - Correction 1

--- printnbr.go
+++ printnbr.go
@@ -6,7 +6,7 @@ func PrintNbr(n int) {
 	slc := []int{}
 	i := 0
 	if n < 0 {
-		n = -1 * n 
+		n = -1 * n
 		z01.PrintRune('-')
 	} else if n <= 0 {
 		z01.PrintRune('0')
@@ -17,6 +17,6 @@ func PrintNbr(n int) {
 		i++
 	}
 	for i := len(slc) - 1; i >= 0; i-- {
-		z01.PrintRune(rune(48 + slc[i])) 
+		z01.PrintRune(rune(48 + slc[i]))
 	}
 }
