2b36d57558768209b694fddea99ec8d58f2647c5
serignmbaye <barsaliou1998@gmail.com>
2022-12-01 00:39:38 +0000

    feat(readme) : create a README.md file for notes

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,6 @@
 key=$(grep "Church" -r interviews -l | cut -d "-" -f2)
+
+#   Same that the previous one
 key1=$(head -n 179 streets/Buckingham_Place | tail -n 1 | cut -d "#" -f2)
 echo $key
 cat interviews/interview-"$key"
