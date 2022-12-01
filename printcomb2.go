a20385616188687d7e51980f0a8fcf7c287d912f
serignmbaye <barsaliou1998@gmail.com>
2022-12-01 17:17:58 +0000

    Print Combination 2 - correction 5

--- printcomb2.go
+++ printcomb2.go
@@ -7,20 +7,31 @@ func PrintComb2() {
 		for j := 0; j <= 9; j++ {
 			for k := 0; k <= 9; k++ {
 				for l := 0; l <= 9; l++ {
-					if i != 9 || j != 9 {
+					if i < k {
 						z01.PrintRune(rune(48 + i))
 						z01.PrintRune(rune(48 + j))
 						z01.PrintRune(' ')
 						z01.PrintRune(rune(48 + k))
 						z01.PrintRune(rune(48 + l))
-						if i != 9 || j != 8 || k != 9 || !(l == 9) {
+						if i != 9 || j != 8 || k != 9 || l != 9 {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
-					}
+					} else if i == k && j < l {
+						z01.PrintRune(rune(48 + i))
+						z01.PrintRune(rune(48 + j))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(48 + k))
+						z01.PrintRune(rune(48 + l))
+						if i != 9 || j != 8 || k != 9 || l != 9 {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					} 
 				}
 			}
 		}
 	}
+	// 23wrF#@Va7D#
 	z01.PrintRune('\n')
 }
