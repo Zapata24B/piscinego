ecf46858dd86ed66e4ee6cd0b4ca06fe06c22d9b
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2022-12-01 22:30:56 +0000

    fix Print Number - 2

--- printnbr.go
+++ printnbr.go
@@ -5,12 +5,10 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	slicedNumber := []int{}
 	index := 0
-	isMinIntValue := false
 	if n < 0 {
 		z01.PrintRune('-')
 		if n == -9223372036854775808 {
 			n = n + 1
-			isMinIntValue = true
 		}
 		n = -1 * n
 	} else if n == 0 {
@@ -22,10 +20,6 @@ func PrintNbr(n int) {
 		index++
 	}
 	for i := len(slicedNumber) - 1; i >= 0; i-- {
-		if isMinIntValue && i == 0 {
-			z01.PrintRune(rune('7'))
-		} else {
-			z01.PrintRune(rune(48 + slicedNumber[i]))
-		}
+		z01.PrintRune(rune(48 + slicedNumber[i]))
 	}
 }
