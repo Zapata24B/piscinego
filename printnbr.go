61e91c24b8ad800a1e00fb36c1cc70ddd109b122
serignmbaye <barsaliou1998@gmail.com>
2022-12-01 19:18:17 +0000

    Print Number - Correction 4

--- printnbr.go
+++ printnbr.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
 	slc := []int{}
-	i := 0
+	index := 0
 	if n < 0 {
 		n = -1 * n
 		z01.PrintRune('-')
@@ -14,7 +14,7 @@ func PrintNbr(n int) {
 	for n > 0 {
 		slc = append(slc, n%10)
 		n = n / 10
-		i++
+		index++
 	}
 	for i := len(slc) - 1; i >= 0; i-- {
 		z01.PrintRune(rune(48 + slc[i]))
