c510a0d296618d7b091c803bd5c77c1fdd11691a
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2022-12-02 22:51:26 +0000

    Atoi

---
+++ atoi.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Atoi(s string) int {
+	number := 0
+	d := 1
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] < '0' || s[i] > '9' {
+			if i != 0 || (s[0] != '-' && s[0] != '+') {
+				return 0
+			}
+		}
+		if (s[i] != '-' && s[i] != '+') {
+			number += (int(rune(s[i])) - 48) * d
+			d = d * 10
+		}
+	}
+	if s[0] == '-' {
+		return -number
+	} else {
+		return number
+	}
+}
