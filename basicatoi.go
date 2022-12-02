c251e3da755fe0a9f0392053b5cfb43deebae0e8
serignmbaye <barsaliou1998@gmail.com>
2022-12-02 19:57:32 +0000

    fix - Basic Atoi

--- basicatoi.go
+++ basicatoi.go
@@ -3,11 +3,9 @@ package piscine
 func BasicAtoi(s string) int {
 	number := 0
 	d := 1
-	
 	for i := len(s) - 1; i >= 0; i-- {
 		number += (int(rune(s[i])) - 48) * d
-		d = d * 10 
+		d = d * 10
 	}
-
 	return number
 }
