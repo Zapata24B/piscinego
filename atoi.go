d5c98c9bce94af963af6320f39302d0b2306c18a
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2022-12-03 00:04:52 +0000

    fix - Atoi - 2

--- atoi.go
+++ atoi.go
@@ -2,21 +2,22 @@ package piscine
 
 func Atoi(s string) int {
 	number := 0
-	d := 1
-	for i := len(s) - 1; i >= 0; i-- {
-		if s[i] < '0' || s[i] > '9' {
-			if i != 0 || (s[0] != '-' && s[0] != '+') {
-				return 0
+	if len(s) > 0 {
+		factor := 1
+		for i := len(s) - 1; i >= 0; i-- {
+			if s[i] < '0' || s[i] > '9' {
+				if i != 0 || (s[0] != '-' && s[0] != '+') {
+					return 0
+				}
+			}
+			if s[i] != '-' && s[i] != '+' {
+				number += (int(rune(s[i])) - 48) * factor
+				factor = factor * 10
 			}
 		}
-		if s[i] != '-' && s[i] != '+' {
-			number += (int(rune(s[i])) - 48) * d
-			d = d * 10
+		if s[0] == '-' {
+			return -number
 		}
 	}
-	if s[0] == '-' {
-		return -number
-	} else {
-		return number
-	}
+	return number
 }
