07d796336572bc5b328ab04a0b6fea9956f4672e
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2022-12-03 16:57:18 +0000

    refactor : optimize some file

--- printcomb.go
+++ printcomb.go
@@ -3,19 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 0; i <= 9; i++ {
-		for j := 0; j <= 9; j++ {
-			for k := 0; k <= 9; k++ {
-				if i != j && i != k && i < j && j < k {
-					if i != 0 || j != 1 || k != 2 {
-						z01.PrintRune(' ')
-					}
-					z01.PrintRune(rune(48 + i))
-					z01.PrintRune(rune(48 + j))
-					z01.PrintRune(rune(48 + k))
-					if i != 7 || j != 8 || k != 9 {
-						z01.PrintRune(',')
-					}
+	for a := 0; a <= 9; a++ {
+		for b := a+1; b <= 9; b++ {
+			for c := b+1; c <= 9; c++ {
+				if a != 0 || b != 1 || c != 2 {
+					z01.PrintRune(' ')
+				}
+				z01.PrintRune(rune(48 + a))
+				z01.PrintRune(rune(48 + b))
+				z01.PrintRune(rune(48 + c))
+				if a != 7 || b != 8 || c != 9 {
+					z01.PrintRune(',')
 				}
 			}
 		}
