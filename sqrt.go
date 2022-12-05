31c15e23027c17f658385f6b1ab78677c77d8b4c
serignmbaye <barsaliou1998@gmail.com>
2022-12-05 15:29:30 +0000

    Square Root

--- sqrt.go
+++ sqrt.go
@@ -1,10 +1,12 @@
 package piscine
 
 func Sqrt(nb int) int {
-	if nb <= 1 {
+	if nb < 1 {
 		return 0
+	} else if nb == 1 {
+		return 1
 	} else {
-		for i := 2; i <= nb/2; i++ {
+		for i := 1; i <= nb/2; i++ {
 			if i*i == nb {
 				return i
 			}
