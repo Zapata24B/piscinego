926da9fb890c6c646f09de574e614e66c7ccf8dd
serignmbaye <barsaliou1998@gmail.com>
2022-12-05 15:16:08 +0000

    Is Prime

--- isprime.go
+++ isprime.go
@@ -7,11 +7,11 @@ func IsPrime(nb int) bool {
 	if nb == 2 {
 		return true
 	}
-	if nb % 2 == 0 {
+	if nb%2 == 0 {
 		return false
 	}
-	for i := 3; i <= nb / 3; i += 2 {
-		if nb % i == 0 {
+	for i := 3; i <= nb/3; i += 2 {
+		if nb%i == 0 {
 			return false
 		}
 	}
