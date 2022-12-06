a002b8696db9a80cf266bead19f93481e5c17430
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 14:36:33 +0000

    Is Lower

---
+++ islower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsLower(s string) bool {
+	runes := []rune(s)
+	isLower := true
+	for i := 0; i < len(s); i++ {
+		if runes[i] <= 'a' || runes[i] >= 'z' {
+			isLower = false
+		}
+	}
+	return isLower
+}
