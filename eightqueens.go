32e37264d51e65413b6803e3f473d26a3f4b4d9d
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 11:10:00 +0000

    Eight Queens

---
+++ eightqueens.go
@@ -0,0 +1,40 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func isSafePosition(lastPosition, testedPosition int, queenPosition [8]int) bool {
+	for i := 0; i < lastPosition; i++ {
+		position := queenPosition[i]
+		if position == testedPosition || position == testedPosition-(lastPosition-i) || position == testedPosition+(lastPosition-i) {
+			return false
+		}
+	}
+	return true
+}
+
+func solve(indexPosition int, queenPosition [8]int) {
+	if indexPosition == 8 {
+		printSolution(queenPosition)
+	} else {
+		for i := 0; i < 8; i++ {
+			if isSafePosition(indexPosition, i, queenPosition) {
+				queenPosition[indexPosition] = i
+				solve(indexPosition+1, queenPosition)
+			}
+		}
+	}
+}
+
+func printSolution(queenPosition [8]int) {
+	for i := 0; i < 8; i++ {
+		z01.PrintRune(rune(queenPosition[i]) + '1')
+	}
+	z01.PrintRune('\n')
+}
+
+func EightQueens() {
+	queenPosition := [8]int{0, 0, 0, 0, 0, 0, 0, 0}
+	solve(0, queenPosition)
+}
