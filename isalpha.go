2f12069594de21c057fd211e7931da873d9a7b52
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 14:48:15 +0000

    Reformat Is Alpha

--- isalpha.go
+++ isalpha.go
@@ -3,9 +3,9 @@ package piscine
 func IsAlpha(s string) bool {
 	runes := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if (runes[i] < 'a' || runes[i] > 'z') {
-			if (runes[i] < 'A' || runes[i] > 'Z') {
-				if (runes[i] < '0' || runes[i] > '1') {
+		if runes[i] < 'a' || runes[i] > 'z' {
+			if runes[i] < 'A' || runes[i] > 'Z' {
+				if runes[i] < '0' || runes[i] > '1' {
 					return false
 				}
 			}
