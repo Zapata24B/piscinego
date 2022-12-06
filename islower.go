2b5decf92ca31e76e41d425d77c2b69234399e1d
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 14:41:34 +0000

    Fix Is Lower

--- islower.go
+++ islower.go
@@ -1,13 +1,10 @@
 package piscine
 
-import "fmt"
-
 func IsLower(s string) bool {
 	runes := []rune(s)
 	isLower := true
 	for i := 0; i < len(s); i++ {
 		if runes[i] < 'a' || runes[i] > 'z' {
-			fmt.Println(string(runes[i]))
 			isLower = false
 		}
 	}
