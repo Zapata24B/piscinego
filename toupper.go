e16d49b968f451c9e1ec8ae6c6a85f2122025119
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 15:10:30 +0000

    Reformat To Upper

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	runes := []rune(s)
 	for i := 0; i < len(s); i++ {
 		if IsLower(string(s[i])) {
-			runes[i] = rune(runes[i]-32)
+			runes[i] = rune(runes[i] - 32)
 		}
 	}
 	return string(runes)
