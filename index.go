849c0fb91f87488ed0b8354b5b2ee6ae9f5addcd
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 14:09:50 +0000

    Index

---
+++ index.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	for i := 0; i < len(s) - len(toFind); i++ {
+		for j := 0; j < len(toFind); j++ {
+			if s[i+j] != toFind[j] {
+				break
+			} else if j == len(toFind)-1 {
+				return i
+			}
+		}
+	}
+	return -1
+}
