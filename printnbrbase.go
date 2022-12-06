70221eb36acd84b735a9f4c368e1c9e78b293440
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 23:00:22 +0000

    Print Nbr Base

---
+++ printnbrbase.go
@@ -0,0 +1,41 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbrBase(nbr int, base string) {
+	isValid := true
+	baseLen := len(base)
+	if baseLen <= 1 {
+		isValid = false
+	}
+	for i := 0; i < baseLen-1; i++ {
+		if base[i] == base[i+1] {
+			isValid = false
+			z01.PrintRune('N')
+			z01.PrintRune('V')
+		}
+	}
+	if isValid {
+		numbers := ""
+		isNegative := false
+		if nbr < 0 {
+			nbr = -nbr
+			isNegative = true
+		}
+		for nbr != 0 {
+			mod := nbr % baseLen
+			numbers += string(base[mod])
+			nbr = nbr / baseLen
+		}
+		numbers = StrRev(numbers)
+		if isNegative {
+			z01.PrintRune('-')
+		}
+		numbersLen := len(numbers)
+		for i := 0; i < numbersLen; i++ {
+			z01.PrintRune(rune(numbers[i]))
+		}
+	}
+}
