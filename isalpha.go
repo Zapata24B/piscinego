ec2ecb5c5a60203431c460b1e25de1596c4b5d38
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 14:50:20 +0000

    Fix - Is Alpha

--- isalpha.go
+++ isalpha.go
@@ -5,7 +5,7 @@ func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
 		if runes[i] < 'a' || runes[i] > 'z' {
 			if runes[i] < 'A' || runes[i] > 'Z' {
-				if runes[i] < '0' || runes[i] > '1' {
+				if runes[i] < '0' || runes[i] > '9' {
 					return false
 				}
 			}
