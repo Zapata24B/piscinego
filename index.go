d3434313738bad51eaf1728fd7cb35f62f2ce49b
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 14:12:14 +0000

    Fix Index

--- index.go
+++ index.go
@@ -1,6 +1,9 @@
 package piscine
 
 func Index(s string, toFind string) int {
+	if len(toFind) == 0 {
+		return -1
+	}
 	for i := 0; i < len(s)-len(toFind); i++ {
 		for j := 0; j < len(toFind); j++ {
 			if s[i+j] != toFind[j] {
