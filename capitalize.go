0be3b4722b6b8d5ae710dbc6475285a077171fe0
serignmbaye <barsaliou1998@gmail.com>
2022-12-06 17:45:39 +0000

    Fix - Capitalize

--- capitalize.go
+++ capitalize.go
@@ -2,23 +2,26 @@ package piscine
 
 func Capitalize(s string) string {
 	runes := []rune(s)
-	isFirstLetterWord := true
-	isOnWord := true
+	isFirstLetterWord := false
+	isOnWord := false
 	for i := 0; i < len(s); i++ {
 		if IsAlpha(string(s[i])) {
 			if isFirstLetterWord && isOnWord {
 				isFirstLetterWord = false
-				isOnWord = true
 			} else if !isFirstLetterWord && !isOnWord {
 				isFirstLetterWord = true
-				isOnWord = true
 			}
+			isOnWord = true
 		} else {
 			isOnWord = false
 		}
-		if isFirstLetterWord && IsLower(string(s[i])) {
-			runes[i] = rune(ToUpper(string(runes[i]))[0])
+		if isFirstLetterWord {
+			if !IsNumeric(string(s[i])) && IsLower(string(s[i])) {
+				runes[i] = rune(ToUpper(string(runes[i]))[0])
+			}
 			isFirstLetterWord = false
+		} else if isOnWord {
+			runes[i] = rune(ToLower(string(runes[i]))[0])
 		}
 	}
 	return string(runes)
