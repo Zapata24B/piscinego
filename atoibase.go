cd8c935541dce7e5b2d66266dad57b5371f04c06
serignmbaye <barsaliou1998@gmail.com>
2022-12-07 14:21:31 +0000

    Atoi Base

--- atoibase.go
+++ atoibase.go
@@ -5,10 +5,21 @@ func AtoiBase(s string, base string) int {
 	if ValidateBase(baseLen, base) {
 		number := 0
 		factor := 1
+		isNegative := false
+		if s[0] == '-' {
+			s = s[1:]
+			isNegative = true
+		}
 		numberLen := len(s)
 		for i := numberLen - 1; i >= 0; i-- {
 			number += Index(base, string(s[i])) * factor
+			if !isNegative && number <= 0 {
+				number = -(number + 1)
+			}
 			factor *= baseLen
+		}
+		if isNegative {
+			number = -number
 		}
 		return number
 	}
