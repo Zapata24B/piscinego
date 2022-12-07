0495477577a9e7864309ae70381b9b54d0ca75de
serignmbaye <barsaliou1998@gmail.com>
2022-12-07 16:13:59 +0000

    Fix - Print Programme

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -12,11 +12,11 @@ func main() {
 	for i := len(filePath) - 1; i >= 0; i-- {
 		if filePath[i] == '/' {
 			fileName = filePath[i+1:]
+			for i := 0; i < len(fileName); i++ {
+				z01.PrintRune(rune(fileName[i]))
+			}
 			break
 		}
-	}
-	for i := 0; i < len(fileName); i++ {
-		z01.PrintRune(rune(fileName[i]))
 	}
 	z01.PrintRune(rune('\n'))
 }
