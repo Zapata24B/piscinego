4eb62c92d6f0d9b3c1a3ee656585a21aacb81e3a
serignmbaye <barsaliou1998@gmail.com>
2022-12-07 22:45:03 +0000

    Fix - Flag

--- flags/main.go
+++ flags/main.go
@@ -9,20 +9,19 @@ import (
 func main() {
 	args := os.Args[1:]
 	numberArgs := len(args)
-	firstArg := args[0]
+	firstArg := ""
+	if numberArgs != 0 {
+		firstArg = args[0]
+	}
 	if numberArgs == 0 || firstArg == "-h" || firstArg == "--help" {
 		fmt.Print("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.\n")
 	} else if firstArg == "-o" || firstArg == "--order" {
 		result := orderTask(args[1])
 		fmt.Println(result)
 	} else if firstArg[:3] == "-i=" || firstArg[:9] == "--insert=" {
-		startInsert := 9
-		if firstArg[:3] == "-i=" {
-			startInsert = 3
-		}
-		insertValue := firstArg[startInsert:]
 		lastArg := args[len(args)-1]
 		nextArg := args[1]
+		insertValue := getInsertValue(firstArg)
 		result := insertTask(lastArg, insertValue)
 		if nextArg == "-o" || nextArg == "--order" {
 			result = orderTask(result)
@@ -31,6 +30,15 @@ func main() {
 	} else {
 		fmt.Println(firstArg)
 	}
+}
+
+func getInsertValue(firstArg string) string {
+	startInsert := 9
+	if firstArg[:3] == "-i=" {
+		startInsert = 3
+	}
+	insertValue := firstArg[startInsert:]
+	return insertValue
 }
 
 func insertTask(str1 string, str2 string) string {
