fa06d5cf5d1ceb9b0fca42cf9b77ff5c7f3e592f
serignmbaye <barsaliou1998@gmail.com>
2022-12-07 16:30:20 +0000

    Fix - Print Programme

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -8,14 +8,9 @@ import (
 
 func main() {
 	filePath := os.Args[0]
-	fileName := ""
-	for i := len(filePath) - 1; i >= 0; i-- {
-		if filePath[i] == '/' {
-			fileName = filePath[i+1:]
-			for i := 0; i < len(fileName); i++ {
-				z01.PrintRune(rune(fileName[i]))
-			}
-			break
+	for _, char := range filePath {
+		if char != '.' && char != '/' {
+			z01.PrintRune(char)
 		}
 	}
 	z01.PrintRune(rune('\n'))
