b3cd3b575eae8cdd0249dac6c886dbf8c29d179d
serignmbaye <barsaliou1998@gmail.com>
2022-12-07 17:53:39 +0000

    Fix - Number Convert Alpha

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -7,26 +7,28 @@ import (
 )
 
 func main() {
-	arguments := os.Args[1:]
-	isUpper := false
-	if arguments[0] == "--upper" {
-		isUpper = true
-		arguments = arguments[1:]
-	}
-	for _, arg := range arguments {
-		number := 0
-		number = basicAtoi(arg)
-		if number >= 1 && number <= 26 {
-			if !isUpper {
-				z01.PrintRune(rune(number + 96))
-			} else {
-				z01.PrintRune(rune(number + 64))
-			}
-		} else {
-			z01.PrintRune(' ')
+	if len(os.Args) >= 2 {
+		arguments := os.Args[1:]
+		isUpper := false
+		if arguments[0] == "--upper" {
+			isUpper = true
+			arguments = arguments[1:]
 		}
+		for _, arg := range arguments {
+			number := 0
+			number = basicAtoi(arg)
+			if number >= 1 && number <= 26 {
+				if !isUpper {
+					z01.PrintRune(rune(number + 96))
+				} else {
+					z01.PrintRune(rune(number + 64))
+				}
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
 	}
-	z01.PrintRune('\n')
 }
 
 func basicAtoi(str string) int {
