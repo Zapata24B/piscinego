a7bfb05fa5e791b2e144fd685429422034875070
serignmbaye <barsaliou1998@gmail.com>
2022-12-07 20:52:03 +0000

    Flag

---
+++ flags/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	// "github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	numberArgs := len(args)
+	firstArg := args[0]
+	if numberArgs == 0 || firstArg == "-h" || firstArg == "--help" {
+		fmt.Print("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.\n")
+	} else if firstArg == "-o" || firstArg == "--order" {
+		result := orderTask(args[1])
+		fmt.Println(result)
+	} else if firstArg[:3] == "-i=" || firstArg[:9] == "--insert=" {
+		startInsert := 9
+		if firstArg[:3] == "-i=" {
+			startInsert = 3
+		}
+		i := 1
+		insertValue := firstArg[startInsert:]
+		lastArg := args[len(args)-1]
+		nextArg := args[1]
+		for i < numberArgs-1 && (nextArg[:2] == "-i" || nextArg[:3] == "--i") {
+			startInsert = 9
+			if nextArg[:3] == "-i=" {
+				startInsert = 3
+			}
+			_insertValue := nextArg[startInsert:]
+			insertValue += _insertValue
+			i++
+			nextArg = args[i]
+		}
+		result := insertTask(insertValue, lastArg)
+		if nextArg == "-o" || nextArg[:7] == "--order" {
+			result = orderTask(result)
+		}
+		fmt.Println(result)
+	} else {
+		fmt.Println(firstArg)
+	}
+}
+
+func insertTask(str1 string, str2 string) string {
+	return str1 + str2
+}
+
+func orderTask(str string) string {
+	runes := []rune(str)
+	for i := range runes {
+		for j := range runes {
+			if runes[i] < runes[j] {
+				runes[i], runes[j] = runes[j], runes[i]
+			}
+		}
+	}
+	return string(runes)
+}
