e75f57a9545258b38cd7b5f87c9f70870b185bd6
serignmbaye <barsaliou1998@gmail.com>
2022-12-08 13:56:27 +0000

    Rotate Vowels

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -8,53 +8,35 @@ import (
 
 func main() {
 	arguments := os.Args[1:]
-
-	var args string
-	var vowels []string
-	var result string
-
-	// S'il n'y a aucun argument, on imprime un saut à la ligne :
-	if len(arguments) < 1 {
+	args := ""
+	vowels := []string(nil)
+	result := ""
+	if len(arguments) >= 1 {
+		for i := 0; i < len(arguments); i++ {
+			args += arguments[i]
+			if i < len(arguments)-1 {
+				args += " "
+			}
+		}
+		for i := 0; i < len(args); i++ {
+			if check(rune(args[i])) {
+				vowels = append(vowels, string(args[i]))
+			}
+		}
+		for i, j := 0, 0; i < len(args); i++ {
+			if check(rune(args[i])) {
+				result += vowels[len(vowels)-j-1]
+				j++
+			} else {
+				result += string(args[i])
+			}
+		}
+		for i := 0; i < len(result); i++ {
+			z01.PrintRune(rune(result[i]))
+		}
+	} else {
 		z01.PrintRune('\n')
 	}
-
-	// On remplit une string ARGS avec chaque argument (+ un espace après chaque mot) :
-	for i := 0; i < len(arguments); i++ {
-		args = args + arguments[i] + " "
-	}
-
-	// On crée un tableau VOWELS contenant les voyelles (bytes castées en string) contenues dans ARGS :
-	for i := 0; i < len(args); i++ {
-		if check(rune(args[i])) {
-			vowels = append(vowels, string(args[i]))
-		}
-	}
-
-	// On inverse la tableau des voyelles :
-	for i := 0; i < len(vowels); i++ {
-		for j := i + 1; j < len(vowels); j++ {
-			vowels[i], vowels[j] = vowels[j], vowels[i]
-		}
-	}
-
-	// On parcourt la string ARGS. Si la lettre est une voyelle, on ajoute sa voyelle "miroir" (inversée) à la string vide RESULT.
-	// Sinon, on ajoute simplement la lettre (consonne) args[i].
-	j := 0
-	for i := 0; i < len(args); i++ {
-		if check(rune(args[i])) {
-			result = result + vowels[j]
-			j++
-
-		} else {
-			result = result + string(args[i])
-		}
-	}
-
-	// On affiche chaque lettre de result :
-	for i := 0; i < len(result); i++ {
-		z01.PrintRune(rune(result[i]))
-	}
-
 }
 
 func check(x rune) bool {
