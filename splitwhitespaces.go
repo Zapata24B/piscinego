79b5d3228f924ba1ffefb51f2fb39bca704c698d
serignmbaye <barsaliou1998@gmail.com>
2022-12-08 11:59:12 +0000

    Fix- Split with white space

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,8 +5,12 @@ func SplitWhiteSpaces(s string) []string {
 	lastWhiteSpace := 0
 	for i := 0; i < len(s); i++ {
 		if s[i] == ' ' {
+			arr = append(arr, s[lastWhiteSpace:i])
+			lastWhiteSpace = i + 1
+		} else if i == len(s)-1 {
 			arr = append(arr, s[lastWhiteSpace:i+1])
 			lastWhiteSpace = i + 1
+
 		}
 	}
 	return arr
