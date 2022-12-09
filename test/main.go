9aaf30f369296e9c8014a48d1279a3687d8a136c
serignmbaye <barsaliou1998@gmail.com>
2022-12-09 11:10:02 +0000

    what to revise

--- test/main.go
+++ test/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	result := piscine.ConvertBase("101011", "01", "0123456789")
-	fmt.Println(result)
+	fmt.Println(piscine.BasicAtoi("12345"))
+	fmt.Println(piscine.BasicAtoi("0000000012345"))
+	fmt.Println(piscine.BasicAtoi("000000"))
 }
