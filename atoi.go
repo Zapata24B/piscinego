1507d827830b1f3fdd19b65aa63e38ab7ac4493b
serignmbaye <barsaliou1998@gmail.com>
2022-12-14 13:41:26 +0000

    exam 2 first part

--- atoi.go
+++ atoi.go
@@ -5,17 +5,15 @@ func Atoi(s string) int {
 	if len(s) > 0 {
 		factor := 1
 		for i := len(s) - 1; i >= 0; i-- {
-			if s[i] < '0' || s[i] > '9' {
-				if i != 0 || (s[0] != '-' && s[0] != '+') {
-					return 0
-				}
+			if (s[i] < '0' || s[i] > '9') && (i != 0 || (s[0] != '-' && s[0] != '+')) {
+				return 0
 			}
 			if s[i] != '-' && s[i] != '+' {
-				number += (int(rune(s[i])) - 48) * factor
+				number += (int(s[i]) - 48) * factor
 				factor = factor * 10
 			}
 		}
-		if s[0] == '-' {
+			if s[0] == '-' {
 			return -number
 		}
 	}
--- strrev.go
+++ strrev.go
@@ -2,10 +2,8 @@ package piscine
 
 func StrRev(s string) string {
 	reverse := []rune(s)
-	j := 0
-	for i := len(s) - 1; i >= 0; i-- {
-		reverse[j] = rune(s[i])
-		j++
+	for i := 0; i < len(s) / 2; i++ {
+		reverse[i], reverse[len(s) - 1 - i] = reverse[len(s) - 1 - i], reverse[i]
 	}
 	return string(reverse)
 }
