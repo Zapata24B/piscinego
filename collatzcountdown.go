3f0eca25c6448da300b4a24c5a8bae4621de186b
serignmbaye <barsaliou1998@gmail.com>
2022-12-15 14:38:48 +0000

    fix - collatw count down

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -4,19 +4,15 @@ func CollatzCountdown(start int) int {
 	if start <= 0 {
 		return -1
 	} else {
-		i := 1
-		for {
+		count := 0
+		for start > 1 {
 			if start%2 == 0 {
 				start /= 2
 			} else {
-				start = (3*start + 1) / 2
+				start = 3*start + 1
 			}
-			i++
-			if start == 1 {
-				i++
-				break
-			}
+			count++
 		}
-		return i
+		return count
 	}
 }
