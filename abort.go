554c6fc9d833eff2b398c0d3bd560872f372ece8
serignmbaye <barsaliou1998@gmail.com>
2022-12-15 12:10:00 +0000

    Fix - Abort

--- abort.go
+++ abort.go
@@ -3,9 +3,9 @@ package piscine
 func Abort(a, b, c, d, e int) int {
 	arr := []int{a, b, c, d, e}
 
-	for i := 0; i < len(arr)-2; i++ {
-		for j := i + 1; j < len(arr)-1; j++ {
-			if arr[i] > arr[j] {
+	for i := range arr {
+		for j := range arr {
+			if arr[i] < arr[j] {
 				arr[i], arr[j] = arr[j], arr[i]
 			}
 		}
