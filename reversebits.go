ba55cb06183f9c3cea2a729ba16aef2288d72911
serignmbaye <barsaliou1998@gmail.com>
2022-12-19 20:46:54 +0000

    Word Match

--- reversebits.go
+++ reversebits.go
@@ -1,16 +1,10 @@
 package piscine
 
-import "fmt"
-
 func ReverseBits(oct byte) byte {
 	result := byte(0)
-	fmt.Printf("%08b\n", oct)
-	bit := oct
 	for i := 0; i < 8; i++ {
-		fmt.Printf("%08b\n", result)
-		bit = (bit >> 1) & 1
-		fmt.Printf("%b\n", bit)
-		result = result | (bit << (7 - i))
+		oct = (oct >> 1) & 1
+		result = result | (oct << (7 - i))
 	}
 	return result
 }
