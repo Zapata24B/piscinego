19b7f91a2adeab523a8064c7908f456d88793002
serignmbaye <barsaliou1998@gmail.com>
2022-12-19 23:55:08 +0000

    List Merge

--- test/main.go
+++ test/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+
 	"piscine"
 )
 
+func PrintList(l *piscine.List) {
+	it := l.Head
+	for it != nil {
+		fmt.Print(it.Data, " -> ")
+		it = it.Next
+	}
+	fmt.Print(nil, "\n")
+}
+
 func main() {
 	link := &piscine.List{}
+	link2 := &piscine.List{}
 
-	piscine.ListPushBack(link, "Hello")
-	piscine.ListPushBack(link, "man")
-	piscine.ListPushBack(link, "how are you")
+	piscine.ListPushBack(link, "a")
+	piscine.ListPushBack(link, "b")
+	piscine.ListPushBack(link, "c")
+	piscine.ListPushBack(link, "d")
+	fmt.Println("-----first List------")
+	PrintList(link)
 
-	for link.Head != nil {
-		fmt.Println(link.Head.Data)
-		link.Head = link.Head.Next
-	}
+	fmt.Println("-----second List------")
+	PrintList(link2)
+
+	fmt.Println("-----Merged List-----")
+	piscine.ListMerge(link, link2)
+	PrintList(link)
 }
