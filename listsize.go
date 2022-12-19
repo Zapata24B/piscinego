e42e0097c759ae27e211ea978a0a1402a0eda9ad
serignmbaye <barsaliou1998@gmail.com>
2022-12-19 21:44:36 +0000

    Reformat - List Size

--- listsize.go
+++ listsize.go
@@ -2,10 +2,14 @@ package piscine
 
 func ListSize(l *List) int {
 	count := 0
+	if l.Head == nil {
+		return 0
+	}
 	actual := l.Head
+	count++
 	for actual.Next != nil {
 		count++
 		actual = actual.Next
 	}
-	return count + 1
+	return count
 }
