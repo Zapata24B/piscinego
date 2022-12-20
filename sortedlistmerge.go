c6a2d49b0eb6100f2a3d6393be79e1efd2fa38a1
serignmbaye <barsaliou1998@gmail.com>
2022-12-20 14:18:39 +0000

    Fix - Sorted List Merge

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -5,7 +5,7 @@ func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
 	head := n1
 	for current := n2; current != nil; {
 		next := current.Next
-		head = SortListInsertNode(n1, current)
+		head = SortListInsertNode(head, current)
 		current = next
 	}
 	return head
