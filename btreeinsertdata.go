d7f1ac89e376d0630349e014b951634d9666090e
serignmbaye <barsaliou1998@gmail.com>
2022-12-20 17:04:19 +0000

    Binairy Search Item

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -9,13 +9,12 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
-
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
-	} else if data == root.Data {
-		root.Parent = BTreeInsertData(root.Parent, data)
-	} else {
+		root.Left.Parent = root
+	} else if data > root.Data {
 		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
 	}
 	return root
 }
--- btreesearchitem.go
+++ btreesearchitem.go
@@ -4,10 +4,8 @@ func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil || root.Data == elem {
 		return root
 	}
-	
 	if elem < root.Data {
 		return BTreeSearchItem(root.Left, elem)
 	}
 	return BTreeSearchItem(root.Right, elem)
-
 }
